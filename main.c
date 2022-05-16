24250cb1f13778203d0c822d7dbee36bb6e83c89
Walter <the.cyber.neo@gmail.com>
2022-05-16 17:18:08 +0300

    b/w r/w

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 typedef unsigned char byte;                     //8 bit
 typedef unsigned short int word;                //16 bit
@@ -21,12 +22,30 @@ word word_read(Adress adr);
 
 
 
-int main() {
-
+void test_mem () {
+    
+    
     byte b0 = 0x0a;
     b_write(2, b0);
     byte bres = b_read(2);
     printf("%02hhx = %02hhx\n", b0, bres);
+    assert(b0 == bres);
+    
+    Adress a = 4;
+    byte b1 = 0x0b;
+    b0 = 0x0a;
+    word w = 0x0b0a;
+    b_write(a, b0);
+    b_write(a+1, b1);
+    word wres = word_read(a);
+    printf("ww/br \t %04hx=%02hhx%02hhx\n", wres, b1, b0);
+    assert(wres == w);
+    
+}
+
+int main() {
+
+    test_mem();
     return 0;
 }
 
@@ -38,4 +57,15 @@ byte b_read(Adress adr) {
     
     return mem[adr];
     
+}
+
+
+word word_read(Adress a) {
+    
+    word w = ((word)mem[a+1]) << 8;
+    w = w | mem[a];
+    printf("w = %x\n", w);
+    
+    return w;
+        
 }

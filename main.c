e712e3f1d968e5daf68834881e94ce9580049999
Walter <the.cyber.neo@gmail.com>
2022-05-16 18:21:57 +0300

    word_write()

--- main.c
+++ main.c
@@ -22,15 +22,15 @@ word word_read(Adress adr);
 
 
 
-void test_mem () {
-    
-    
+void test_mem() {
+    printf("%s", "\nTesting b_write\n");
     byte b0 = 0x0a;
     b_write(2, b0);
     byte bres = b_read(2);
     printf("%02hhx = %02hhx\n", b0, bres);
     assert(b0 == bres);
     
+    printf("%s", "\nTesting b_write of word\n");
     Adress a = 4;
     byte b1 = 0x0b;
     b0 = 0x0a;
@@ -41,10 +41,15 @@ void test_mem () {
     printf("ww/br \t %04hx=%02hhx%02hhx\n", wres, b1, b0);
     assert(wres == w);
     
+    printf("%s", "\nTesting word_write\n");
+    word m = 0x0c0b;
+    word_write(8, m);
+    wres = word_read(8);
+    printf("%04hx\n", wres);
+    assert(wres == m);
 }
 
 int main() {
-
     test_mem();
     return 0;
 }
@@ -64,8 +69,14 @@ word word_read(Adress a) {
     
     word w = ((word)mem[a+1]) << 8;
     w = w | mem[a];
-    printf("w = %x\n", w);
+    printf("w = 0%x\n", w);
     
     return w;
         
+}
+
+void word_write(Adress adr, word w)
+{
+    mem[adr+1] = (byte) (w >> 8);
+    mem[adr] = (byte) (w);
 }

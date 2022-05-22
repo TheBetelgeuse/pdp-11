3997785a85d00508a676ae7c3f9993cf74a9fd6e
Walter <the.cyber.neo@gmail.com>
2022-05-22 19:57:35 +0300

    Final version

--- main.c
+++ main.c
@@ -1,107 +1,77 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <assert.h>
 #include <stdarg.h>
-#include "do_func.c"
-
-#define MEMSIZE (64*1024)
-#define pc reg[7]
-
+#include "pdp-11.h"
 
 
 byte mem[MEMSIZE];
 word reg[8];
-
+Arg ss, dd, b_flag, r, nn, xx;
+char N_flag, Z_flag, V_flag, C_flag;
 int trace_flag = 0;
+word odata = 0177564;
+word oscr = 0177566;
 
 
-
-
-
-//void trace(char * format, ...) {
-//    if (trace_flag){
-//        va_list ptr;
-//    }
-//}
-void test_mem() {
-    printf("%s", "\nTesting b_write\n");
-    byte b0 = 0x0a;
-    b_write(2, b0);
-    byte bres = b_read(2);
-    printf("%02hhx = %02hhx\n", b0, bres);
-    assert(b0 == bres);
-    
-    printf("%s", "\nTesting b_write of word\n");
-    Adress a = 4;
-    byte b1 = 0x0b;
-    b0 = 0x0a;
-    word w = 0x0b0a;
-    b_write(a, b0);
-    b_write(a+1, b1);
-    word wres = word_read(a);
-    printf("ww/br \t %04hx=%02hhx%02hhx\n", wres, b1, b0);
-    assert(wres == w);
-    
-    printf("%s", "\nTesting word_write\n");
-    word m = 0x0c0b;
-    word_write(8, m);
-    wres = word_read(8);
-    printf("%04hx\n", wres);
-    assert(wres == m);
+byte b_read (adr a) {
+    if (a == odata)
+        return 1;
+    if (a <= 7)
+        return (byte) reg[a];
+    else
+        return mem[a];
 }
 
-
-void b_write(Adress adr, byte b) {
-    
-    mem[adr] = b;
+void b_write (adr a, byte val) {
+    if (a > 7) {
+        mem[a] = val;
+    } else {
+        if (val >> (LEN_BYTE - 1))
+            reg[a] = val + 0xff00;
+        else
+            reg[a] = val;
+    }
+    if (a == oscr)
+        printf("%c", b_read(a));
 }
-byte b_read(Adress adr) {
-    
-    return mem[adr];
-    
-}
 
-
-word word_read(Adress a) {
-    
-    word w = ((word)mem[a+1]) << 8;
+word w_read(adr a) {
+    if (a == odata)
+        return 1;
+    word w = ((word) mem[a + 1]) << LEN_BYTE;
     w = w | mem[a];
-
-    
     return w;
-        
 }
 
-void word_write(Adress adr, word w)
-{
-    mem[adr+1] = (byte) (w >> 8);
-    mem[adr] = (byte) (w);
+void w_write (adr a, word val) {
+    if (a <= 7) {
+        reg[a] = val;
+    } else {
+        mem[a + 1] = (byte) (val >> LEN_BYTE);
+        mem[a] = (byte) (val);
+    }
+    if (a == oscr)
+        printf("%c", b_read(a));
 }
 
+void print_bin(unsigned char x, char end){
+    char a[LEN_BYTE+1] = {};
 
-//void load_file(const char * filename) {
-//
-//
-//    int adress, num;
-//    Adress adr, n;
-//    byte a;
-//    FILE *data;
-//    data = fopen(filename, "r");
-//    fscanf(data,"%d %d", &adress, &num);
-//    adr = (Adress) adress;
-//    n = (Adress) num;
-//    for (Adress i = 0; i < n; i++){
-//        fscanf(data, "%hhx", &a);
-//        b_write(adr+i, a);
-//    }
-//
-//}
+    for(int i = LEN_BYTE-1; x > 0 && i >= 0; i--) {
+        a[i] = x % 2;
+        x = x / 2;
+    }
+    for(int i = 0; i < LEN_BYTE; i++)
+        printf("%d", a[i]);
+    if (end)
+        printf("%c", end);
+}
 
 void load_file(const char *file) {
-    Adress a, n;
+    adr a, n;
     byte k;
 
     FILE *f = fopen(file, "r");
@@ -111,27 +81,18 @@ void load_file(const char *file) {
     }
 
     while (fscanf(f, "%hx%hx", &a, &n) != EOF) {
-        for (Adress i = 0; i < n; i++) {
+        for (adr i = 0; i < n; i++) {
             fscanf(f, "%hhx", &k);
             b_write(a + i, k);
         }
     }
     fclose(f);
 }
-void run(){
-    trace("----------running-----------\n");
-    pc = 01000;
-    while(1) {
-        
-        word w = word_read(pc);
-        trace("%06o %06o: ", pc, w);
-        pc+=2;
-        int i = 0;
-        while ((w & cmd[i].mask) != cmd[i].opcode) {
-            i++;
-        }
-        trace("%s\t\n", cmd[i].name);
-        cmd[i].do_func();
+
+
+void mem_dump(adr start, word n) {
+    for (int i = 0; i < n; i += 2) {
+        printf("%06o : %06o\n", start + i, w_read(start + i));
     }
 }
 
@@ -148,6 +109,13 @@ void trace(char * format, ...) {
         va_end(ptr);
     }
 }
+
+void print_reg() {
+    for (int i = 0; i < 8; i++) {
+        trace("r%o = %06o ;\n", i, reg[i]);
+    }
+}
+
 int main(int argc, char * argv[]) {
     if (argc == 1) {
         usage(argv[0]);
@@ -155,9 +123,9 @@ int main(int argc, char * argv[]) {
     }
     for (int i = 1; i < argc; ++i) {
         if (argv[i][0] == '-') {
-            if (argv[i][1] == 't'){
+            if (argv[i][1] == 't')
                 trace_flag = 1;
-            }else {
+            else {
                 usage(argv[0]);
                 return 1;
             }
